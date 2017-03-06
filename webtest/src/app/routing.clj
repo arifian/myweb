a50fdc3c44df6b9ff34bb75937e7b79411348806
arifian <arifian.r@gmail.com>
2017-03-06 17:37:11 +0700

    asdf

--- webtest/src/app/routing.clj
+++ webtest/src/app/routing.clj
@@ -5,13 +5,15 @@
             [hiccup.core :as hc]
             [app.api :as api]))
 
+(def common-interceptors [(body-params/body-params) http/html-body])
+
 (def routes
   (route/expand-routes
-   #{["/" :get [(bd/body-params) http/html-body api/home-main]]
-     #_["/new" :get [(bd/body-params) http/html-body new-post]]
-     #_["/ok" :post [(bd/body-params) http/html-body create-post]]
-     #_["/okedit/:postid" :post [(bd/body-params) http/html-body edit-post-ok]]
-     #_["/post/:postid" :get [(bd/body-params) http/html-body view-post]]
-     #_["/edit/:postid" :get [(bd/body-params) http/html-body edit-post]]
-     #_["/delete/:postid" :get [(bd/body-params) http/html-body delete-post]]
-     #_["/deleteok/:postid" :post [(bd/body-params) http/html-body delete-post-ok]]}))
+   #{["/" :get [common-interceptors api/home-main]]
+     #_["/new" :get [common-interceptors new-post]]
+     #_["/ok" :post [common-interceptors create-post]]
+     #_["/okedit/:postid" :post [common-interceptors edit-post-ok]]
+     #_["/post/:postid" :get [common-interceptors view-post]]
+     #_["/edit/:postid" :get [common-interceptors edit-post]]
+     #_["/delete/:postid" :get [common-interceptors delete-post]]
+     #_["/deleteok/:postid" :post [common-interceptors delete-post-ok]]}))
