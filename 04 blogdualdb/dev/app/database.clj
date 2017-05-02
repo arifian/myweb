3dc088ed4b250b48abb403e26e7253df2f98e354
arifian <arifian.r@gmail.com>
2017-05-02 18:39:20 +0700

    some docstring

--- 04 blogdualdb/dev/app/database.clj
+++ 04 blogdualdb/dev/app/database.clj
@@ -1,2 +1,31 @@
-(ns app.database)
+(ns app.database
+  (:require [app.database.abstraction :as abstr]))
 
+;database function calls
+
+#_(defn createdb [name]
+    (abstr/-createdb name))
+
+(defn initschema [dt]
+  (abstr/-initschema dt))
+
+(defn getallpost [db]
+  (abstr/-getallpost db))
+
+(defn addpost [db title content]
+ (abstr/-addpost db title content))
+
+(defn addsample [db]
+  (abstr/-addsample db))
+
+(defn editpost [db postkey postid title content]
+  (abstr/-editpost db postkey postid title content))
+
+(defn removepost [db postkey]
+  (abstr/-removepost db postkey))
+
+(defn startdb [db]
+  (abstr/-startdb db))
+
+(defn stopdb [db]
+  (abstr/-stopdb db))
